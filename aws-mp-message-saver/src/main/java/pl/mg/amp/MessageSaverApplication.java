fb69303befe138cd1f5b7ce2016c1f7dc659f9bf
maciejgz <macgzi@gmail.com>
2024-01-07 18:55:55 +0100

    #2 java message service stub

---
+++ aws-mp-message-saver/src/main/java/pl/mg/amp/MessageSaverApplication.java
@@ -0,0 +1,13 @@
+package pl.mg.amp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageSaverApplication.class, args);
+    }
+
+}
