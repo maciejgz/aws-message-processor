5384bd738cad07c2d877b2ce55f9321a726f005c
maciejgz <macgzi@gmail.com>
2024-01-20 22:21:19 +0100

    #2 java message service
    - retrieving secret WIP

--- aws-mp-message-saver/src/main/java/pl/mg/amp/MessageSaverApplication.java
+++ aws-mp-message-saver/src/main/java/pl/mg/amp/MessageSaverApplication.java
@@ -1,11 +1,15 @@
 package pl.mg.amp;
 
+import com.amazonaws.secretsmanager.caching.SecretCache;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class MessageSaverApplication {
 
 
@@ -19,6 +23,19 @@ public class MessageSaverApplication {
     @PostConstruct
     public void init() {
         System.out.println("Secret variable: " + secretVariable);
+        this.getSecret();
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void performTask() {
+        System.out.println("Performing a task...");
+    }
+
+    private final SecretCache cache  = new SecretCache();
+    public String getSecret() {
+        final String secret  = cache.getSecretString("secret.variable");
+        System.out.println("Secret: " + secret);
+        return null;
     }
 
 }
